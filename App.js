64280a00b81cec7c666be85a7438dd1bdd7cf7f0
rezhaibrahim <rezhaibrahim96@gmail.com>
2021-02-15 11:06:17 +0700

    base application

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import {Provider} from 'react-redux';
+// import {PersistGate} from 'redux-persist/integration/react';
+// import Router from './src/router/router';
+// import store from './src/redux/store';
+import {View,Text} from 'react-native'
+export default function App() {
+  return (
+   
+   <View>
+     <Text>Hello world</Text>
+   </View>
+  );
+}
---
+++ assets/index.js
@@ -0,0 +1,40 @@
+export const Assets = {
+    home: {
+        active: require('./beranda_active.png'),
+        inactive: require('./beranda_inactive.png'),
+    },
+    cinema: {
+        active: require('./bioskop_active.png'),
+        inactive: require('./bioskop_inactive.png'),
+    },
+    ticket: {
+        active: require('./tiket_active.png'),
+        inactive: require('./tiket_inactive.png'),
+    },
+    promo_1: require('./promo_1.png'),
+    promo_2: require('./promo_2.png'),
+    promo_3: require('./promo_3.png'),
+    profile_icon: require('./profile_icon.png'),
+    location_icon: require('./location.png'),
+    arrow_down_icon: require('./arrow_down.png'),
+    logo: require('./logo.png'),
+    poster: {
+        poster_1: require('./poster/1.png'),
+        poster_2: require('./poster/2.png'),
+        poster_3: require('./poster/3.png'),
+        poster_4: require('./poster/4.png'),
+        buy_ticket: require('./poster/buy_ticket.png'),
+    },
+    news: {
+        news_1: require('./news/1.png'),
+        news_2: require('./news/2.png'),
+        news_3: require('./news/3.png'),
+        news_4: require('./news/4.png'),
+        eye_icon: require('./news/eye_icon.png'),
+        thumb_icon: require('./news/thumb_icon.png'),
+    },
+    arrow_right: require('./arrow_right.png'),
+    ticket_bg: require('./ticket_bg.png'),
+    qr: require('./qr.png'),
+
+}
